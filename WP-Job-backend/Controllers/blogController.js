f56855da880dd7e16a84b674539bbc95dcdc83e7
haamid10 <apdixamiid919@gmail.com>
2023-09-07 16:21:10 +0300

    added blogs

--- WP-Job-backend/Controllers/blogController.js
+++ WP-Job-backend/Controllers/blogController.js
@@ -27,9 +27,41 @@ exports.getAll = async (req, res) => {
     }
     }
 
-// Compare this snippet from WP-Job-backend\Controllers\commentController.js:
-// const commentModel = require("../Models/commentModel");
-//
-// exports.createComment = async (req, res) => {
-//  try {
+// delete blog
+
+exports.deleteBlog = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteBlog = await log.findByIdAndDelete({ _id: id })
+        res.status(200).json({ message: deleteBlog  ,data:"deleted"})
+    }
+    catch (error) {
+        res.status(400).json(error.message)
+    }
+}
+
+// update blog
+exports.updateBlog =async (req,res)=>{
+    try{
+        // console.log('hello')
+        const {id}= req.params;
+        // console.log(id);
+        // console.log(req.body,"this is body");
+        // console.log(typeof(req.body))
+        // console.log((Object.keys(req.body)))
+        const updateblog=await log.findOneAndUpdate({_id :id})
+        // console.log(updatedblog)
+        res.status(200).json({message:updated, data: updateblog})
+    }
+    catch(error){
+        res.status(400).json(error)
+    }
+}
+    
+
+
+
+
+
+
 
--- WP-Job-backend/Routes/blogRoute.js
+++ WP-Job-backend/Routes/blogRoute.js
@@ -4,5 +4,9 @@ const blogController = require("../Controllers/blogController")
 const upload = require("../upload/Multer")
 router.post("/create",upload.single("image"), blogController.createBlog);
 router.route("/").get(blogController.getAll)
+router
+    .route("/:id")
+    .delete(blogController.deleteBlog)
+    .put(upload.single("image"),blogController.updateBlog)
 
 module.exports= router;
