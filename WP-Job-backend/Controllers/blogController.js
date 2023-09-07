bfdd0900066d662c722e2558d2b5225e099370d4
haamid10 <apdixamiid919@gmail.com>
2023-09-07 14:34:13 +0300

    added blogs

--- WP-Job-backend/Controllers/blogController.js
+++ WP-Job-backend/Controllers/blogController.js
@@ -3,31 +3,29 @@ const log = require("../Models/blogModel")
 
 
 exports.createBlog = async (req, res) => {
-    try {
-    //   req.body.image = req.file.filename;
-      console.log(req.body);
-      const blog = await log.create(req.body);
-      res.status(200).json({ message: "Blog successfully added." , data: blog });
-    } catch (error) {
-      res.status(400).json({ message: error.message });
+        try {
+            const blog = await log.create(req.body)
+            res.status(200).json({
+                status: "success",
+                data: blog
+            })
+        } catch (error) {
+            res.status(400).json({
+                // status: "failed",
+                message: error.message
+            })
+        }
     }
-  };
-   
-    // exports.createBlog = async (req, res) => {
-    //     try {
-    //         const blog = await blogPost.create(req.body)
-    //         res.status(200).json({
-    //             status: "success",
-    //             data: blog
-    //         })
-    //     } catch (error) {
-    //         res.status(400).json({
-    //             status: "failed",
-    //             message: error
-    //         })
-    //     }
-    // }
 
+exports.getAll = async (req, res) => {
+    try {
+        const getAll= await log.find({}).populate("user")
+        res.status(200).json({message: getAll})
+    }
+    catch(error){
+        res.status(400).json(error.message)
+    }
+    }
 
 // Compare this snippet from WP-Job-backend\Controllers\commentController.js:
 // const commentModel = require("../Models/commentModel");
--- WP-Job-backend/Routes/blogRoute.js
+++ WP-Job-backend/Routes/blogRoute.js
@@ -2,6 +2,7 @@ const express = require ("express");
 const router = express.Router();
 const blogController = require("../Controllers/blogController")
 const upload = require("../upload/Multer")
-router.post("/create", blogController.createBlog)
+router.post("/create",upload.single("image"), blogController.createBlog);
+router.route("/").get(blogController.getAll)
 
 module.exports= router;
